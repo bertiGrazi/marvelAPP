056265188c6b5af25fb4beb7e5f17a10b801d6ab
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-11-12 17:52:55 -0300

    feat: inicial files project and storyboard deletion

---
+++ marvelAPP/marvelAPP/Controller/CharactersViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  marvelAPP
+//
+//  Created by Grazi  Berti on 12/11/22.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
